93dfbf4d6dc73583eb206cc3d3d59c02cde4fbd5
IgnacioTejeraPicossi <ignacio.tejera@outlook.com>
2024-03-25 12:36:37 +0100

    change in BowlingGamet

---
+++ src/test/java/io/rockmoongames/bowlingstar/BowlingGameTest.java
@@ -0,0 +1,22 @@
+package io.rockmoongames.bowlingstar;
+
+import org.junit.jupiter.api.Test;
+
+public class BowlingGameTest {
+
+    @Test
+    public void gregTestOne() {
+
+
+    }
+
+    @Test
+    public void gregTestTwo() {
+
+    }
+
+    public void gregBOKTestThree() {
+
+    }
+
+}
